e73076712afec75d9898d696d7f04332e86df342
antonsmit30 <antonsmit3045@gmail.com>
2018-11-15 11:32:02 +0200

    error handling pages integrated

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -3,4 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from my_project.core.views import core
+from my_project.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
